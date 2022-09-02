4af4f19bbe2e0b9ff1b0b85855e40f3b881e8934
jepens <atlastitried@gmail.com>
2022-09-02 22:25:22 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-916qu6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-916qu6)
---
+++ script.js
@@ -0,0 +1,58 @@
+// tangkap element absensi_form dan root
+let absensi_form = document.getElementById('absensi_form');
+let root = document.getElementById('root');
+
+// kita buat array untuk menampung data absensi
+let absensi_data = [];
+
+// tambahkan event listener submit ke element absensi_form
+absensi_form.addEventListener('submit', (event) => {
+  //mencegah form dari reload page
+  event.preventDefault();
+
+  let fullname = event.target.fullname.value;
+
+  // console.info(fullname);
+
+  //validasi mini
+  if (fullname == '') {
+    alert('isi dulu bambang');
+    return;
+  }
+
+  // push data ke dalam array absensi data
+  absensi_data.push({
+    nama_lengkap: fullname,
+    waktu: new Date().toLocaleTimeString(),
+    tanggal: new Date().toLocaleDateString(),
+  });
+
+  // reset input field
+  event.target.fullname.value = '';
+
+  //panggil function render html
+  renderToHtml();
+});
+
+//function untuk render data array ke div root
+function renderToHtml() {
+  root.innerHTML = '';
+
+  //mapping array to html element
+  absensi_data.forEach((e, i) => {
+    root.innerHTML += `
+<div class="card" draggable="true" ondragend="handleDelete(${i})" >
+<span> ${i} . &nbsp; ${e.nama_lengkap} </span>
+<span> ${e.waktu} ${e.tanggal} </span>
+</div>
+`;
+  });
+}
+
+// delete function
+function handleDelete(index) {
+  //delete 1 data dari array
+  absensi_data.splice(index, 1);
+  // render kembali ke html
+  renderToHtml();
+}
